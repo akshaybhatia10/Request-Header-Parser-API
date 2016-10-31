7ba7aa01804fb6ffc39d5e41697f00d5aee74b09
Akshay Bhatia <akshaybhatia10@gmail.com>
2016-10-31 12:49:14 +0000

    Added handling

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express"),
+    app     = express();
+    
+app.set("view engine", "ejs");
+app.get("/",function(req,res){
+    
+    res.redirect("/whoami");
+});
+
+app.get("/whoami",function(req,res){
+    var ipaddress = req.headers['x-forwarded-for'] ||
+                   req.connection.remoteAddress;
+    var language = req.headers["accept-language"].split(",")[0];
+    var software = req.headers["user-agent"].match(/\((.*?)\)/)[1];
+    
+   res.json({"ipaddress":ipaddress,"language":language,"software":software}); 
+   
+
+
+});
+    
+app.listen(process.env.PORT, process.env.IP);
