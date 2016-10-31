09216181cb9dc283dc162041ead00f2877086efc
Akshay Bhatia <akshaybhatia10@gmail.com>
2016-10-31 13:14:30 +0000

    Label added

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require("express"),
     app     = express();
     
+(function() {
+	// Get page title
+  	var pageTitle = ("Request-Header-Parser-API").text();
+
+	
+	// Change page title back on focus
+	(window).focus(function() {
+	  ("title").text(pageTitle);
+	});
+});    
+    
 app.get("/",function(req,res){
     
     res.redirect("/whoami");
+    
 });
 
 app.get("/whoami",function(req,res){
